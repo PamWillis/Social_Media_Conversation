64a5758552d8983c4035db18fc0d01b90b999379
Pam <pwillisop@yahoo.com>
2023-11-25 18:56:43 -0500

    brought in reaction as a subdocument

--- models/Thought.js
+++ models/Thought.js
@@ -1,34 +1,64 @@
 // Define Mongoose
 const { Schema, model } = require('mongoose');
 
-const thoughtSchema = new Schema(
+const reactionSchema = new Schema(
     {
-    thoughtText: { 
-    type: String,
-    required: true,
-    min_length: 1,
-    max_length: 280,
-},
-createdAt: {
-    type: Date,
-    imutable: true,
-    default: () => Date.now(),
-},
-username: {
-type: String,
-required: true,
-},
-},
-{
-    toJSON: {
-        virtuals: true,
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: new Types.ObjectId(),
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280,
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal),
+      },
     },
-    id: false,
-});
-
+    {
+      toJSON: {
+        getters: true,
+      },
+    }
+  );
+  
+  const thoughtSchema = new Schema(
+    {
+      thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal),
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      reactions: [reactionSchema],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true,
+      },
+      id: false,
+    }
+  );
 
 thoughtSchema.virtual("reactionCount").get(function () {
     return `${this.reactions.length}`;
 })
-const Thought = model('thought', thoughtSchema);
+const Thought = model('Thought', thoughtSchema);
 module.exports = Thought;
