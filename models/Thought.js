d18e2cea8274128f8a3991101fa427b24cf35eff
Pam <pwillisop@yahoo.com>
2023-11-25 18:06:02 -0500

    corrected query for thought and user

--- models/Thought.js
+++ models/Thought.js
@@ -28,7 +28,7 @@ required: true,
 
 
 thoughtSchema.virtual("reactionCount").get(function () {
-    return this.reactions.length;
+    return `${this.reactions.length}`;
 })
 const Thought = model('thought', thoughtSchema);
 module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -42,7 +42,7 @@ const userSchema = new Schema(
 
 
 userSchema.virtual("friendCount").get(function () {
-    return this.friends.length;
+    return `${this.friends.length}`;
 });
 
 
