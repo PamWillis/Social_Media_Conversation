422273c38a6926f4da4bf6c21fa2e721f18a0e43
Pam <pwillisop@yahoo.com>
2023-11-26 11:30:22 -0500

    finding routes

---
+++ utils/seed.js
@@ -0,0 +1,39 @@
+//require in database connection
+import { faker } from '@faker-js/faker'
+
+const { Thought } = require('./Thought');
+const { User } = require('./User');
+//Require in the model names ie const {Thought} = require("file path here")
+//open database and delete everything in it
+db.once('open', async () => {
+    await Thought.deleteMany({})
+    await User.deleteMany({})
+    //Inside the same function - create fake data and add to model
+    const generateFakeUser = () => {
+        return {
+            id: faker.string.uuid(),
+            email: faker.internet.email(),
+            thoughts: faker.string.uuid(),
+            friends: faker.string.uuid(),
+        }
+    }
+    const generateFakeThought = () => {
+        return {
+            id: faker.string.uuid(),
+            thoughtText: faker.lorem.lines(),
+            createdAt: faker.date.past(),
+            username: faker.internet.userName(),
+            reaction: faker.lorem.lines(),
+        }
+    }
+    await User.collection.insertMany(generateFakeUser)
+    Thought.create({generateFakeThought, generateFakeThought})
+    console.log("FINISHED SEEDING")
+    process.exit(0);
+    
+    //Create fake data
+    //Add to DB --> using await User.collection.insertMany(arrayOfDataYouMadeHere)
+    //Now for thoughts you can use the mongo function Thought.create({datahere, datahere})
+    //Maybe a console log once it's all done console.log("FINISHED SEEDING")
+    //Exit process.exit(0);
+}) //Close the db.open function
